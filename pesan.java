06e356e033fe42c35ff47e4949f535dea0f31143
tika <kurniasari28novi@gmail.com>
2024-04-24 15:08:42 +0800

    membuat menu

---
+++ pesan.java
@@ -0,0 +1,10 @@
+package pesan;
+
+public class pesan {
+    public void daftar() {
+        System.out.println("Daftar Makanan:");
+        System.out.println("1. Nasi Goreng");
+        System.out.println("2. Mie Goreng");
+        System.out.println("3. Soto Ayam");
+    }
+}
