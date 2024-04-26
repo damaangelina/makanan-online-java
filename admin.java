3d589880193b19924fe369247ceb5f051fc12825
tikakurnsri <147170238+tikakurnsri@users.noreply.github.com>
2024-04-27 01:09:03 +0800

    Update admin.java
    admin setengah

--- admin.java
+++ admin.java
@@ -1,3 +1,15 @@
-public class admin {
-}
+import java.util.Scanner;
 
+public class admin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println("\nSelamat datang, admin!");
+            System.out.println("1. Lihat menu restoran");
+            System.out.println("2. Tambah menu restoran");
+            System.out.println("3. Hapus menu");
+            System.out.println("4. Keluar");
+            System.out.print("Pilih menu: ");
+            int choice = scanner.nextInt();
